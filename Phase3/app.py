8eb6de60014c26438f1c3e60e3dbce8609549362
Rolando <74209303+rbcuellar98@users.noreply.github.com>
2022-11-07 00:23:59 -0500

    UI change

--- Phase3/app.py
+++ Phase3/app.py
@@ -2,13 +2,13 @@ import dash.dependencies
 import dash_daq as daq
 from dash import html, Input, Output, dcc, Dash, State
 import dash_bootstrap_components as dbc
-from dash_boostrap_templates import ThemeChangerAIO
+from dash_bootstrap_templates import ThemeChangerAIO
 import RPi.GPIO as GPIO
 import time
 import Freenove_DHT as DHT
 import mail_client as email
 from datetime import datetime
-#import motor as motor
+
 
 GPIO.setmode(GPIO.BCM) # BCM
 GPIO.setwarnings(False)
@@ -18,19 +18,7 @@ EMAIL_SEND = False
 
 DHTPin = 17 
 dht = DHT.DHT(DHTPin) #create a DHT class object
-#Phase3 pin
-intensityPin = 13 #33 in GPIO.BOARD
-GPIO.setup(intensityPin, GPIO.OUT)
-# This works as long as the arduino code is running (change broker)
-broker = '192.168.1.11'
-port = 1883
-topic = "/IoTlab/Server"
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
-app = Dash(__name__)
-img = html.Img(src=app.get_asset_url('lightOffp'),width='100px', height='100px')
-humidityValue = 0
-temperatureValue = 0
+dht.readDHT11()
 
 Motor1 = 27
 Motor2 = 18
@@ -45,75 +33,63 @@ def startMotor():
     GPIO.output(Motor3,GPIO.HIGH)
     time.sleep(10)
     GPIO.output(Motor1, GPIO.LOW)
+
+#Phase3 pin
+intensityPin = 13 #33 in GPIO.BOARD
+GPIO.setup(intensityPin, GPIO.OUT)
+
+
+app = Dash(__name__)
+img = html.Img(src=app.get_asset_url('lightOffp'),width='100px', height='100px')
+humidityValue = 0
+temperatureValue = 0
+
 theme_change = ThemeChangerAIO(aio_id="theme")
 
-app.layout = html.Div(children=[
-    html.H1(children='Dashboard', style={'text-align': 'center', 'margin-left': '6%', 'margin-bottom': '5%'}),
-    html.Div( id='led-box', style={'margin-left': '6%', 'margin-top': '5%', 'margin-bottom': '5%'},children=[
-        html.H1(children='LED Controller'),
-        html.Button(img, id='led-image', n_clicks = 0),
-    ]),
+offcanvas = html.Div(
+    [
+        dbc.Button(
+            "Profile", id="open-offcanvas-backdrop",style={'padding': 10, 'border': 'none', 'background': 'none'}
+        ),
+        dbc.Offcanvas(
+            html.Div(
+            [                       
+                html.Div(style={'text-align': 'center'},children=[
+                      html.Img(src=app.get_asset_url('avatar.png'), width='50%', height='50%', style={'border-radius': '50%'})
+            ]),
+                dbc.Row(
+                [
+                    dbc.Col(html.Div("Name: ")),
+                    dbc.Col(html.Div(dbc.Input(placeholder="username", size="md", className="mb-3", readonly=True))),
+                ]),
+                dbc.Row(
+                [
+                    dbc.Col(html.Div("Ideal Temperature: ")),
+                    dbc.Col(html.Div(dbc.Input(placeholder="ideal_temp", size="md", className="mb-3", readonly=True))),
+                ]),
+                dbc.Row(
+                [
+                    dbc.Col(html.Div("Ideal Humidity: ")),
+                    dbc.Col(html.Div(dbc.Input(placeholder="ideal_humidity", size="md", className="mb-3", readonly=True))),
+                ]),
+                dbc.Row(
+                [
+                    dbc.Col(html.Div("Ideal light intensity: ")),
+                    dbc.Col(html.Div(dbc.Input(placeholder="ideal_light_intensity", size="md", className="mb-3", readonly=True))),
+                ]),
+            ]),
+            id="offcanvas-backdrop",
+            title="User Options",
+            is_open=False,
+        ),
+    ]
+)
 
-    html.Div(className='grid-container', id='humidity-and-temperature', style={'margin-left': '2%', 'margin-top': '5%', 'margin-bottom': '5%'}, children=[
-        html.H1(children='Humidity & Temperature'),
-    ]),
 
-
-    dbc.Row([
-        dbc.Col(html.Div(id='humidity', children=[
-            daq.Gauge(
-            color={"gradient":True,"ranges":{"yellow":[0,30],"green":[30,50],"red":[50,100]}},
-            id='humidity-gauge',
-            label='Current Humidity',
-            showCurrentValue=True,
-            units="Percentage",
-            value=humidityValue,
-            max=100,
-            min=0
-            )
-        ])),
-        dbc.Col(html.Div(id='temperature', children=[
-            daq.Thermometer(
-            id='temperature-thermometer',
-            label='Current temperature',
-            value=temperatureValue,
-            showCurrentValue=True,
-            min=0,
-            max=100,
-            style={
-                'margin-top': '5%',
-                'margin-bottom': '5%'
-            })])),  
-        dbc.Col(html.Div(id='dc-motor', children=[
-            daq.ToggleSwitch(
-            id='motor',
-            label=['Fan On', 'Fan Off'],
-            value='',
-            #if temperatureValue >= 24:
-            #    GPIO.output(Motor1,GPIO.HIGH)
-            #    GPIO.output(Motor2,GPIO.LOW)
-            #    GPIO.output(Motor3,GPIO.HIGH)
-            #    value=False
-            #elif temperatureValue < 24:
-            #    sleep(5)
-            #    GPIO.output(Motor1,GPIO.LOW)
-            #    GPIO.cleanup()
-            #    value=motorOn
-            style={
-                'margin-top': '5%',
-                'margin-bottom': '5%'
-            }
-            
-            
-            )])) 
-        ]),
- 
-    dcc.Interval(id='interval-component', interval=1*5000, n_intervals=0)
-],  style={'backgroundColor':'#727174', 'padding-top': '2%'})
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(theme_change, style={'padding': 0, 'border': 'none', 'background': 'none'}),
+        dbc.NavItem(theme_change, style={'padding': 10, 'border': 'none', 'background': 'none'}),
         # dbc.NavItem(dbc.NavLink("Settings", href="#")),
         dbc.NavItem(dbc.NavLink(offcanvas))
 
@@ -123,10 +99,90 @@ navbar = dbc.NavbarSimple(
     color="dark",
     dark=True,
     sticky='top'
+    
+    
 )
+ledBoxTab = html.Div(id='led-box', className='ledBox',children=[
+                html.H1(children='LED'),
+                html.Button(img, id='led-image', n_clicks = 0),
+        ])
+
+humidTempTab = html.Div(className='grid-container', children=[
+                dbc.Row([
+                    dbc.Col(html.Div(id='humidity', children=[
+                        daq.Gauge(
+                        color={"gradient":True,"ranges":{"yellow":[0,30],"green":[30,50],"red":[50,100]}},
+                        id='humidity-gauge',
+                        label='Current Humidity',
+                        showCurrentValue=True,
+                        units="Percentage",
+                        value=humidityValue,
+                        max=100,
+                        min=0
+                        )
+                    ])),
+                    dbc.Col(html.Div(id='temperature', children=[
+                        daq.Thermometer(
+                        id='temperature-thermometer',
+                        label='Current temperature',
+                        value=temperatureValue,
+                        showCurrentValue=True,
+                        min=0,
+                        max=100
+                        )
+                    ]))        
+                ])
+            ])
+
+fanControlTab =  html.Div(className='grid-container',children=[
+                html.Div(style={'text-align': 'center'},children=[
+                    html.Img(src=app.get_asset_url('fan.png'), width='15%', height='15%')
+                ]),
+                daq.ToggleSwitch(
+                size=100,
+                id='fan-toggle',
+                value=False,
+                label='Fan Status',
+                labelPosition='bottom',
+                color = '#0C6E87', 
+                style={
+                    'margin-top': '3%'
+                }
+                ),
+            ])
+
+tabs = dcc.Tabs([
+        dcc.Tab(className='custom-tab', id='LED', label='LED Control', children=[
+            ledBoxTab
+        ]),
+        dcc.Tab(className='custom-tab', id='HT', label='Humidity & Temperature', children=[
+            humidTempTab
+        ]),
+        dcc.Tab(className='custom-tab',id='FC', label='Fan Control', children=[
+            fanControlTab
+        ]),
+    ], colors={
+        "border": "#097966",
+        "primary": "#097966",
+        "background": "#3cdfff"
+    })
+
+
+
+app.layout = html.Div(id="theme-switch-div", children=[
+    navbar,
+    html.H1(children='DASHBOARD', style={'text-align': 'center', 'margin-top': '3%'}),
+
+    html.Div(className='grid-container', style={'margin': '5% 7% 5% 7%'}, children=[
+        tabs
+    ]),
+    dcc.Interval(id='interval-component', interval=1*1500, n_intervals=0)
+])
+
+
 @app.callback(Output('led-image', 'children'),
               Input('led-image', 'n_clicks')
-              )
+                )
 def update_output(n_clicks):
     if n_clicks % 2 == 1:
         GPIO.output(_ledPin, GPIO.HIGH)
@@ -137,14 +193,24 @@ def update_output(n_clicks):
         img = html.Img(src=app.get_asset_url('lightOffp'),width='100px', height='100px')
         return img
     
-def main():
-    app.run_server(debug=True, host='localhost', port=8050)
+
+@app.callback(Output('fan-toggle', 'value'),
+              Input('fan-toggle', 'value')
+)
+def toggle_fan(value):
+    if value:
+        GPIO.output(Motor1, GPIO.HIGH)
+        value = True
+    else:
+         GPIO.output(Motor1, GPIO.LOW)
+         value = False
+    return value
 
 
 @app.callback(Output('humidity-gauge', 'value'),
               Output('temperature-thermometer', 'value'),
               Input('interval-component', 'n_intervals'))
-    
+
 def update_sensor(n):
     global EMAIL_SEND
     dht.readDHT11()
@@ -166,12 +232,23 @@ def update_sensor(n):
             startMotor()
             
     return humidityValue, temperatureValue
-            
-    #turning the motor off when the temperature is lower than 24
-#    if temperatureValue < 24:
-#        motor.stop()
-#        email_sent = False
-    
+ 
 
+#Phase 3 code
+@app.callback(
+    Output("offcanvas-backdrop", "is_open"),
+    Input("open-offcanvas-backdrop", "n_clicks"),
+    State("offcanvas-backdrop", "is_open"))
+def toggle_offcanvas(n1, is_open):
+    if n1:
+        return not is_open
+    return is_open
+
+
+
+
+
+def main():
+    app.run_server(debug=True, host='localhost', port=8060)
+   
 main()
-
